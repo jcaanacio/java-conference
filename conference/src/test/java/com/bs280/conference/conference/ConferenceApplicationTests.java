a895b7a46a23d1a483094787f49c69d0fad906b2
JC <jcaanacio@gmail.com>
2023-03-14 01:03:10 +0800

    build(core): initial commit

---
+++ conference/src/test/java/com/bs280/conference/conference/ConferenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bs280.conference.conference;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
